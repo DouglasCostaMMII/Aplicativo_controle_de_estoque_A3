da3953452c22a144998e4d2b55e4a3a68243166a
Felipe Ghizoni <felipeghizoni01@gmail.com>
2024-10-12 13:41:52 -0300

    feat&fix: Adição dos templates produtos, relatorios e categorias e mais conceitos de clean code aplicados

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ from Model.saidasDAO import SaidasDAO
 from Model.produtosDAO import ProdutosDAO 
 
 # Muda o diretório de templates para 'view'
-app = Flask(__name__, template_folder='view')
+app = Flask(__name__, template_folder='View')
 
 # Criação de objetos que serão posteriormente utilizados 
-categoria = CategoriaDAO()
-entrada = EntradasDAO()
-saidas = SaidasDAO()
-produtos = ProdutosDAO()
+categoria_Obj = CategoriaDAO()
+entrada_Obj = EntradasDAO()
+saidas_Obj = SaidasDAO()
+srodutos_Obj = ProdutosDAO()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -22,8 +22,39 @@ def index():
             return redirect(url_for('main'))
         else:
             show_error = True
-            return render_template('index.html', show_error=show_error)
-    
+            return render_template('categorias.html', show_error=show_error)
+
+# Função responsável por carregar o template de produtos:        
+@app.route('/produtos', methods=['GET', 'POST'])
+def produtos():
+    results = None
+    if banco_conectado():
+        results = ""
+        return render_template('produtos.html', envios=results)
+    else:
+        return "Erro na conexão com o banco de dados"
+
+# Função responsável por carregar o template de categorias:      
+@app.route('/categorias', methods=['GET', 'POST'])
+def categorias():
+    results = None
+    if banco_conectado():
+        results = ""
+        return render_template('categorias.html', envios=results)
+    else:
+        return "Erro na conexão com o banco de dados"
+
+# Função responsável por carregar o template de relatórios:      
+@app.route('/relatorios', methods=['GET', 'POST'])
+def relatorios():
+    results = None
+    if banco_conectado():
+        results = ""
+        return render_template('relatorios.html', envios=results)
+    else:
+        return "Erro na conexão com o banco de dados"
+
+# Função responsável por verificar se há conexão com o banco de dados:         
 def banco_conectado():
     # Parâmetros para se conectar ao banco de dados. Atenção ao IP do host e a senha do banco utilizados. 
     db_config = {
@@ -33,7 +64,6 @@ def banco_conectado():
         'database': "estoque"
     }
     try: 
-        print("Tentando")
         conn = mysql.connector.connect(**db_config)
         conn.close()
         return True
@@ -44,7 +74,7 @@ def banco_conectado():
 @app.route('/main', methods=['GET', 'POST'])
 def main():
     if banco_conectado():
-        return render_template('index.html')
+        return render_template('categorias.html')
     else:
         return "Erro na conexão com o banco de dados"
 
