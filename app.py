e5b89802bb14e4aaf1b0b4cf0ab0ca1a76773155
Felipe Ghizoni <felipeghizoni01@gmail.com>
2024-11-06 19:58:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for
-from Model.categorias import Categoria 
-from Model.entradas import Entradas 
+from Model.categorias import Categoria
+from Model.entradas import Entradas
 from Model.saidas import Saidas
 from Model.produtos import Produtos
 from Controller.conexaoDAO import ConexaoDAO
 
+
+
+
 # Muda o diretório de templates para 'View' e o static para 'Estilo'
 app = Flask(__name__, template_folder='View', static_folder='View/Estilo')
 
-# Criação de objetos que serão posteriormente utilizados 
+
+# Criação de objetos que serão posteriormente utilizados
 categoria_Obj = Categoria()
 entrada_Obj = Entradas()
 saidas_Obj = Saidas()
 produtos_Obj = Produtos()
 conexao = ConexaoDAO()
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -24,6 +29,7 @@ def index():
         else:
             show_error = True
             return render_template('produtos.html', show_error=show_error)
+
 
 ''' Produtos '''
 # Função responsável por carregar o template de produtos:        
@@ -48,27 +54,29 @@ def add_produto():
     mensagem_erro = "Erro ao cadastrar produto"
     mensagem_sucesso = "Produto cadastrado com sucesso"
 
+
     if acao == "cancelar":
         return redirect(url_for('produtos'))
-    
+   
     # tradamento de dados
     if "," in preco:
         preco = preco.replace(",", ".")
 
+
     if not (nome and status and categoria and preco and qnt_min) and acao == "confirmar":
         return render_template('produtos.html', mensagem_alerta=mensagem_alerta)  # Caso não sejam passados dados
-    
+   
     categorias = categoria_Obj.visualizarCategoria()    
     if any(categoria_return['nome'] == categoria for categoria_return in categorias):
         categoria = categoria_Obj.getCategoriaid(categoria)
     else:
         return render_template('produtos.html', error_message="Categoria selecionada não existe")  # Caso categoria não exista
-    
+   
     if acao == "confirmar" and produtos_Obj.add_produto(nome, status, categoria, preco, qnt_min, acao)[0]:
         return render_template('produtos.html', mensagem_sucesso=mensagem_sucesso)  # Caso de sucesso no processo
     else:
         return render_template('produtos.html', error_message=mensagem_erro)  # Caso de erro no processo
-    
+   
 # Função para editar produtos
 @app.route('/editar_produto', methods=['POST'])
 def editar_produto():
@@ -83,18 +91,20 @@ def editar_produto():
     mensagem_erro = "Erro ao alterar dados do produto"
     mensagem_sucesso = "Produto alterado com sucesso"
 
+
     if acao == "cancelar":
         return redirect(url_for('produtos'))
 
+
     if not (nome and status and categoria and preco and qnt_min) and acao == "confirmar":
         return render_template('produtos.html', mensagem_alerta=mensagem_alerta)  # Caso não sejam passados dados
-    
+   
     categorias = categoria_Obj.visualizarCategoria()    
     if any(categoria_return['nome'] == categoria for categoria_return in categorias):
         categoria = categoria_Obj.getCategoriaid(categoria)
     else:
         return render_template('produtos.html', error_message="Categoria selecionada não existe")  # Caso categoria não exista
-    
+   
     if acao == "confirmar" and produtos_Obj.editar_produto(nome, status, categoria, preco, qnt_min, produtoid)[0]:
         # tradamento de dados
         if "," in preco:
@@ -102,7 +112,7 @@ def editar_produto():
         return render_template('produtos.html', mensagem_sucesso=mensagem_sucesso)  # Caso de sucesso no processo
     else:
         return render_template('produtos.html', error_message=mensagem_erro)  # Caso de erro no processo# tradamento de dados
-    
+   
 # Função para mover produtos
 @app.route('/mover_produto', methods=['POST'])
 def mover_produto():
@@ -114,30 +124,32 @@ def mover_produto():
     mensagem_erro_quantia = "Quantidade selecionada tornará o estoque negativo! selecione outra quantia"
     mensagem_erro = "Erro ao alterar a quantidade de estoque"
     mensagem_sucesso = "Quantidade alterada com sucesso"
-    
+   
     # Caso nem todos os campos estejam preenchidos retorna mensagem de erro ao usuário
     if not (tipo_movimentacao and quantidade ) and acao == "confirmar":
         return render_template('produtos.html', mensagem_alerta=mensagem_alerta)
     # Caso operação seja cancelada recarrega a página
     elif acao == "cancelar":
         return redirect(url_for('produtos'))
-    
+   
     elif acao == "confirmar":
         quantidade_atual = produtos_Obj.getQuantidade(produtoid)
         diferenca = quantidade_atual - quantidade
 
+
         # Caso subtração seja negativa (ERRO)
-        if tipo_movimentacao == "RETIRADA" and (diferenca < 0): 
+        if tipo_movimentacao == "RETIRADA" and (diferenca < 0):
             return render_template('produtos.html', mensagem_erro_quantia=mensagem_erro_quantia)  
         # Caso subtração seja positiva (OK)
         elif tipo_movimentacao == "RETIRADA" and (diferenca >= 0):
             if produtos_Obj.setQuantidade(produtoid, diferenca):
-                return render_template('produtos.html', mensagem_sucesso=mensagem_sucesso)   
+                return render_template('produtos.html', mensagem_sucesso=mensagem_sucesso)  
         # Caso a operação seja de adição
         elif tipo_movimentacao == "ADIÇÂO" and produtos_Obj.setQuantidade(produtoid, (quantidade_atual+ quantidade)):
                 return render_template('produtos.html', mensagem_sucesso=mensagem_sucesso)  
     else:
-        return render_template('produtos.html', mensagem_erro=mensagem_erro) 
+        return render_template('produtos.html', mensagem_erro=mensagem_erro)
+
 
 # Função para alterar o status
 @app.route('/alterar_StatusProduto', methods=['POST'])
@@ -157,6 +169,7 @@ def alterar_StatusProduto():
     else:
         return "Erro ao editar o produto", 500  # Caso de erro no processo
 
+
 ''' Categorias '''
 # Função responsável por carregar o template de categorias:      
 @app.route('/categorias', methods=['GET', 'POST'])
@@ -168,6 +181,7 @@ def categorias():
     else:
         return "Erro na conexão com o banco de dados"
 
+
 # Função responsável por carregar o template de relatórios:      
 @app.route('/relatorios', methods=['GET', 'POST'])
 def relatorios():
@@ -178,6 +192,7 @@ def relatorios():
     else:
         return "Erro na conexão com o banco de dados"
 
+
 def fetch_produtos_data():
     results = produtos_Obj.visualizar_produtos()
     # Atualizando o dicionário com o nome correspondente ao ID do cliente
@@ -186,5 +201,7 @@ def fetch_produtos_data():
             obj['categoria_id'] = categoria_Obj.getNome(obj['categoria_id'])
     return results
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
