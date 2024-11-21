595ca8f4d0b05406ea0b51ac73cee72d54a297ab
Felipe Ghizoni <felipeghizoni01@gmail.com>
2024-11-21 20:28:19 -0300

    Update conexaoDAO.py

--- Model/conexaoDAO.py
+++ Model/conexaoDAO.py
@@ -1,31 +1,24 @@
 import mysql.connector
-import sqlite3
 import socket
 
 
 class ConexaoDAO:
-
     def get_ip(self):
         hostname = socket.gethostname()  # Obtém o nome do host
         ip_address = socket.gethostbyname(hostname)  # Obtém o IP do host
         return ip_address
     
     def dados_db(self):
-        return {
-            'user': 'root',
-            'password': "",
-            'host': self.get_ip(),
-            'database': "estoque"
-        }
+        return {'user': 'root',
+                'password': "",
+                'host': self.get_ip(),
+                'database': "estoque"}
 
     def banco_conectado(self):
         db_config = self.dados_db()
-        
         try: 
             conn = mysql.connector.connect(**db_config)
             return [True, conn]
         except mysql.connector.Error as err:
             print(f"Falha na conexão: {err}")
             return [False, None]
-        
-    
