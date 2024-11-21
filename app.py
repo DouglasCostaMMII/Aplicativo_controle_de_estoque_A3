59477e9900a4635be51a5e1c88d173d3e0cd319a
Felipe Ghizoni <felipeghizoni01@gmail.com>
2024-11-21 20:26:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, redirect, url_for
-from Controller.produtosAPP import produtos_blueprint  # Importe o blueprint de produtos
+from Controller.produtosAPP import produtos_blueprint  
 from Controller.categoriasAPP import categorias_blueprint
 from Controller.relatoriosAPP import relatorio_blueprint
 
 # Inicializa o app Flask
 app = Flask(__name__, template_folder='View/Corpo', static_folder='View/Estilo')
 
-# Registra o blueprint
+# Registra os blueprints
 app.register_blueprint(produtos_blueprint)
 app.register_blueprint(categorias_blueprint)
 app.register_blueprint(relatorio_blueprint)
@@ -18,4 +18,3 @@ def index():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-    
